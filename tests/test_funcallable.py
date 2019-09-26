f028d0846f9195bcfd4db18c0fbe33dc3eb375c2
frank <frank.chang@shoufuyou.com>
2019-09-26 22:44:16 +0800

    add  new  decorations

---
+++ tests/test_funcallable.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*- 
+"""
+@User     : Frank
+@File     : test_funcallable.py
+@DateTime : 2019-09-26 21:45 
+@Email    : frank.chang@lexisnexis.com
+
+pytest  -v -s  tests/test_funcallable.py
+
+"""
+
+from useful_decoration.func_callable import checked_arguments
+
+from useful_decoration.func_callable import default_value
+
+import pytest
+
+
+def check_a(data):
+    a = data.get('a')
+
+    if a >= 10:
+        return True
+    return False
+
+
+@checked_arguments(callback=check_a, default=default_value)
+def calculate(a):
+    """
+    用户函数
+    :return:
+    """
+    return {"ret": 20}
+
+
+
+
+
+
+"""
+对于这种 的例子 
+"""
+
+
+
+
+
+
+def test_calculate():
+    assert 20 == calculate(a=10).get('ret')
+
+
+def test_calculate_02():
+    assert 0 == calculate(a=1).get('ret')
+
+
+if __name__ == '__main__':
+    pytest.main(["-s", "test_funcallable.py"])
+    pass
